cb16cdef84ad5c4e5f3197d2edcb689183c2fcde
ChanhyukPark-Tech <69495129+ChanhyukPark-Tech@users.noreply.github.com>
2022-10-05 09:05:53 +0900

    initialize: jump into cnubot spring server (#1)
    * jump into cnubot spring server
    
    * chore: .github template

---
+++ src/main/java/com/cnubot/cnubotserver/CnubotServerApplication.java
@@ -0,0 +1,13 @@
+package com.cnubot.cnubotserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnubotServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnubotServerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/cnubot/cnubotserver/domain/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.cnubot.cnubotserver.domain.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *  버스와 충남대 홈페이지 소식 게시글 관련 Controller입니다.
+ */
+
+@RestController
+public class MainController {
+}
