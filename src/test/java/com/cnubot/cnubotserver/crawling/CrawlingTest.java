d4cb9343271e0269b0caa5721298b159c103320b
kingseungwooya <ksw_0505@nate.com>
2022-10-27 23:45:17 +0900

    test: refactoring test
    crawling refactoring한 결과 test

--- src/test/java/com/cnubot/cnubotserver/crawling/CrawlingTest.java
+++ src/test/java/com/cnubot/cnubotserver/crawling/CrawlingTest.java
@@ -1,4 +1,19 @@
 package com.cnubot.cnubotserver.crawling;
 
+
+import com.cnubot.cnubotserver.domain.service.BoardCrawling;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class CrawlingTest {
+    @Autowired
+    BoardCrawling boardCrawling;
+
+    @Test
+    void 크롤링테스트(){
+        boardCrawling.process();
+    }
+
 }
