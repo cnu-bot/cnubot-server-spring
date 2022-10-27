88fee054d0c07f08319ce9e258893952ad512847
kingseungwooya <ksw_0505@nate.com>
2022-10-27 23:12:17 +0900

    test: jayspt
    VM option으로 argument값 받아서 JasyptEncrypt PASSWORD 암호화 후 테스트 완료

---
+++ src/test/java/com/cnubot/cnubotserver/JasyptConfigTest.java
@@ -0,0 +1,54 @@
+package com.cnubot.cnubotserver;
+
+import org.assertj.core.api.Assertions;
+import org.hibernate.cfg.Environment;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+public class JasyptConfigTest {
+    @Value("${jasypt.encryptor.password}")
+    private String PASSWORD;
+    @Autowired
+    Environment environment;
+    @Test
+    void jasypt(){
+
+        String url = "jdbc:mysql://localhost:3306/cnubot?useSSL=false&useUnicode=true&serverTimezone=Asia/Seoul";
+        String username = "root";
+        String password = "1234";
+        //PASSWORD = System.getProperty("jasypt.encryptor.password");
+        System.out.println(PASSWORD);
+
+        String encryptUrl = jasyptEncrypt(url);
+        String encryptUsername = jasyptEncrypt(username);
+        String encryptPassword = jasyptEncrypt(password);
+
+        System.out.println("encryptUrl : " + encryptUrl);
+        System.out.println("encryptUsername : " + encryptUsername);
+        System.out.println("encryptPassword : " + encryptPassword);
+
+        Assertions.assertThat(url).isEqualTo(jasyptDecryt(encryptUrl));
+    }
+
+    private String jasyptEncrypt(String input) {
+        // Example key
+        PASSWORD = System.getProperty("jasypt.encryptor.password");
+        System.out.println(PASSWORD);
+        String key = PASSWORD;
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        encryptor.setAlgorithm("PBEWithMD5AndDES");
+        encryptor.setPassword(key);
+        return encryptor.encrypt(input);
+    }
+
+    private String jasyptDecryt(String input){
+        // Example key
+        String key = PASSWORD;
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        encryptor.setAlgorithm("PBEWithMD5AndDES");
+        encryptor.setPassword(key);
+        return encryptor.decrypt(input);
+    }
+}
