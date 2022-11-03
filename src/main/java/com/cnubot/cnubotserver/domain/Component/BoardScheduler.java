3460b60555fd8a588db933496a8f1fbb374e5734
김승우 <86772586+kingseungwooya@users.noreply.github.com>
2022-11-03 23:27:06 +0900

    feat: Board Service 개발 (#9)
    * feat: Board Entity 설계
    
    Board Entity설계와 enumtype으로 크롤링 url 열거
    
    * rename: yml
    
    properties-> yml
    yml 설정
    
    * feat: Board Service
    
    add: jsoup dependency
    ,Board service
    
    * feat: Schedular
    
    Board Service에 Schedular 적용
    main에서 시작과 즉시 크롤링 해와야 하기에 CommandLineRunner구현
    
    * feat: swagger
    
    fix: error BoardService
    
    * refactor: Change Injection
    
    Autowired에서 Constructor Injection으로 불변성을 지킴.
    
    * feat: Add dependency jasypt
    
    * feat: Set jasypt
    
    DB 정보 보호를 위한 jasypt 설정
    
    * test: jayspt
    
    VM option으로 argument값 받아서 JasyptEncrypt PASSWORD 암호화 후 테스트 완료
    
    * refactor: snakecase to camelcase
    
    변수 명 camel case로 통일
    
    * refactor: refactoring production code
    
    가독성을 높이기 위한 refactoring
    
    * test: refactoring test
    
    crawling refactoring한 결과 test
    
    * chore: change encrpt(local -> server)
    
    도커에 올린 ec2서버의 주소와 계정으로 암호화 완료: vm option 양식
    -Djasypt.encryptor.password= PW
    PW 는 디비 PW와 동일
    
    * feat: add Builder annotation
    
    생성자 파라미터 주입을 명확하게 하기 위해 빌더패턴 사용
    
    * fix: fix testcase
    
    테스트케이스 수정
    
    * refact: change repeating setter to builder pattern
    
    빌더패턴으로 가독성 높임과 반복되는 setter 제거
    
    * chore: remove blank line
    
    * fix: fix wrong encrypt url
    
    * refact: refactor method input type for getViewDetail
    
    getViewDetail함수에서도 getDoc을 사용하기 위해 String형태의 url로 인자 변경
    
    * fix: fix Board Entity
    
    요구사항에 맞게 게시글의 내용을 추출한 내용을 저장하기 위한 속성 생성
    
    * feat: add getViewDetail
    
    각 게시글마다 내용을 추출하는 기능
    Builder 생성자 객체에 변경된 entity형식에 맞게 boardDetail 추가
    
    * fix: BoardCrawling Service
    
    새로운 속성 boardDetail을 구하기 위한 보수작업
    
    * fix: yml
    
    encrpt된 부분 오류 수정
    test 로그 확인을 위해 불필요 로그 제거
    
    * test: getViewDetail
    
     getViewDetail test
    
    * fix: entity size
    
    244바이트 넘어가는 내용에 의해 Lob 어노테이션 추가
    
    * feat: runner
    
    서버 시작시  자동으로 크롤링 데이터 생성
    
    * refact: deleteAll to deleteAllInBatch
    
    deleteAll 사용시 한 행 삭제할때마다 쿼리문이 날라가기에 deleteAllinBatch로 refactoring

---
+++ src/main/java/com/cnubot/cnubotserver/domain/Component/BoardScheduler.java
@@ -0,0 +1,20 @@
+package com.cnubot.cnubotserver.domain.Component;
+
+import com.cnubot.cnubotserver.domain.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class BoardScheduler {
+
+    private final BoardService boardService;
+
+    // cron 방식으로 스케쥴링 1시간 간격으로 refresh
+    @Scheduled(cron = "0 0/30 * 1/1 * ? ")
+    public void refresh(){
+        boardService.refreshBoards();
+    }
+}
--- src/main/java/com/cnubot/cnubotserver/domain/repository/RepositoryJpa.java
+++
@@ -1,6 +0,0 @@
-package com.cnubot.cnubotserver.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RepositoryJpa extends JpaRepository<String, String> {
-}
