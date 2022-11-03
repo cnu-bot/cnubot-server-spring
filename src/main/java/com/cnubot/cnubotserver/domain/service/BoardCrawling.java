0cc7712a7bed872657aa1e59eb21ca5b5c648ff1
kingseungwooya <ksw_0505@nate.com>
2022-11-03 13:11:55 +0900

    refact: change repeating setter to builder pattern
    빌더패턴으로 가독성 높임과 반복되는 setter 제거

--- src/main/java/com/cnubot/cnubotserver/domain/service/BoardCrawling.java
+++ src/main/java/com/cnubot/cnubotserver/domain/service/BoardCrawling.java
@@ -53,27 +53,33 @@ public class BoardCrawling {
         if(menu.ordinal()<7){
             elements = document.select("td");
             for (int i = 0; i < elementsSize - 6; i += 6) {
-                board.setMenu(menu);
-                board.setBoardNum(elements.get(i).text()); //번호
-                board.setUrl(elements.get(i + 1).childNode(0).attr("href")); //url
-                board.setName(elements.get(i + 1).text()); //제목
-                board.setWriter(elements.get(i + 2).text()); //적송저
-                board.setDate(elements.get(i + 3).text()); //작성일
-                board.setHits(elements.get(i + 4).text()); //조회수
+                String boardUrl = elements.get(i + 1).childNode(0).attr("href");
+                board = Board.builder()
+                        .menu(menu)
+                        .boardNum(elements.get(i).text())
+                        .date(elements.get(i + 3).text())
+                        .writer(elements.get(i + 2).text())
+                        .hits(elements.get(i + 4).text())
+                        .url(boardUrl)
+                        .name(elements.get(i + 1).text())
+                        .build();
                 repository.save(board);
             }
         }
         else if(menu.ordinal()<10){
             elements = document.select("td");
             for (int i = 0; i < elementsSize - 6; i += 6) {
-                board.setMenu(menu);
-                board.setBoardNum(elements.get(i).text()); //번호
-                board.setUrl(elements.get(i + 1).childNode(0).attr("href")); //url
-                board.setName(elements.get(i + 1).text()); //제목
-                board.setWriter(elements.get(i + 2).text()); //작성자
-                board.setPeriod(elements.get(i + 3).text()); //모집기한
-                board.setDate(elements.get(i + 4).text()); //작성일
-                board.setHits(elements.get(i + 5).text()); //조회수
+                String boardUrl = elements.get(i + 1).childNode(0).attr("href");
+                board = Board.builder()
+                        .menu(menu)
+                        .boardNum(elements.get(i).text())
+                        .date(elements.get(i + 4).text())
+                        .writer(elements.get(i + 2).text())
+                        .hits(elements.get(i + 5).text())
+                        .url(boardUrl)
+                        .name(elements.get(i + 1).text())
+                        .period(elements.get(i + 3).text())
+                        .build();
                 repository.save(board);
             }
         }
@@ -81,28 +87,33 @@ public class BoardCrawling {
             elements = document.select(".bodo_listThum");
 
             for (int i = 0; i < elementsSize; i++) {
-                board.setMenu(menu);
-                board.setUrl(elements.get(i).child(0).attr("href"));
-                board.setPicUrl(elements.get(i).child(0).child(0).attr("src"));
-                board.setName(elements.get(i).getElementsByTag("h4").text());
-                board.setBoardNum("");
-                board.setWriter(replace(elements.get(i).getElementsByTag("li").get(0).text()));
-                board.setDate(replace(elements.get(i).getElementsByTag("li").get(1).text()));
-                board.setHits(elements.get(i).getElementsByTag("li").get(2).text());
+                String boardUrl = elements.get(i).child(0).attr("href");
+                board = Board.builder()
+                        .menu(menu)
+                        .date(replace(elements.get(i).getElementsByTag("li").get(1).text()))
+                        .writer(replace(elements.get(i).getElementsByTag("li").get(0).text()))
+                        .hits(elements.get(i).getElementsByTag("li").get(2).text())
+                        .url(boardUrl)
+                        .name(elements.get(i).getElementsByTag("h4").text())
+                        .picUrl(elements.get(i).child(0).child(0).attr("src"))
+                        .build();
                 repository.save(board);
             }
         }
         else {
             elements = document.select("tr");
             for (int i = 1; i < elements.size(); i++) {
-                board.setMenu(menu);
                 Element element = elements.get(i);
-                board.setBoardNum(element.getElementsByClass("b-num-box").text());
-                board.setDate(element.getElementsByClass("b-date").text());
-                board.setWriter(element.getElementsByClass("b-writer").text());
-                board.setHits(element.getElementsByClass("hit").text().split(" ")[1]);
-                board.setUrl(menu.getPort() + element.getElementsByTag("a").attr("href"));
-                board.setName(element.getElementsByTag("a").text());
+                String boardUrl = menu.getPort() + element.getElementsByTag("a").attr("href");
+                board = Board.builder()
+                        .menu(menu)
+                        .boardNum(element.getElementsByClass("b-num-box").text())
+                        .date(element.getElementsByClass("b-date").text())
+                        .writer(element.getElementsByClass("b-writer").text())
+                        .hits(element.getElementsByClass("hit").text().split(" ")[1])
+                        .url(boardUrl)
+                        .name(element.getElementsByTag("a").text())
+                        .build();
                 repository.save(board);
             }
         }
