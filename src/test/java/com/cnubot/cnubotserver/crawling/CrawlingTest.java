afce9ee4940f2e2715ddbb3d2e0f9da7709cf895
kingseungwooya <ksw_0505@nate.com>
2022-11-03 18:59:55 +0900

    test: getViewDetail
     getViewDetail test

--- src/test/java/com/cnubot/cnubotserver/crawling/CrawlingTest.java
+++ src/test/java/com/cnubot/cnubotserver/crawling/CrawlingTest.java
@@ -16,4 +16,10 @@ public class CrawlingTest {
         boardCrawling.process();
     }
 
+    @Test
+    void 게시글내용추출테스트(){
+        String result =
+                boardCrawling.getViewDetail("https://plus.cnu.ac.kr/_prog/_board/?mode=V&no=2489272&code=sub07_0703&site_dvs_cd=kr&menu_dvs_cd=0703&skey=&sval=&site_dvs=&ntt_tag=&GotoPage=");
+        System.out.println(result);
+    }
 }
