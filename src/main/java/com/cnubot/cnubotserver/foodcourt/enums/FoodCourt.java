d728e2ae2996c9e4d782d4c067f9fa3213f13853
kingseungwooya <ksw_0505@nate.com>
2022-11-19 19:34:38 +0900

    fix: fix enums

--- src/main/java/com/cnubot/cnubotserver/foodcourt/enums/FoodCourt.java
+++ src/main/java/com/cnubot/cnubotserver/foodcourt/enums/FoodCourt.java
@@ -1,7 +1,10 @@
 package com.cnubot.cnubotserver.foodcourt.enums;
 
 public enum FoodCourt {
-    STUDENT_HALL("https://mobileadmin.cnu.ac.kr/food/index.jsp"),
+    SECOND_STUDENT_HALL("https://mobileadmin.cnu.ac.kr/food/index.jsp?searchLang=OCL04.10&searchView=date&searchCafeteria=OCL03.02"),
+    THIRD_STUDENT_HALL("https://mobileadmin.cnu.ac.kr/food/index.jsp?searchLang=OCL04.10&searchView=date&searchCafeteria=OCL03.03"),
+    FOURTH_STUDENT_HALL("https://mobileadmin.cnu.ac.kr/food/index.jsp?searchLang=OCL04.10&searchView=date&searchCafeteria=OCL03.04"),
+    COLLEGE_OF_LIFE_SCIENCE("https://mobileadmin.cnu.ac.kr/food/index.jsp?searchLang=OCL04.10&searchView=date&searchCafeteria=OCL03.05"),
     DORMITORY("https://dorm.cnu.ac.kr/html/kr/sub03/sub03_0304.html");
 
     private final String port;
