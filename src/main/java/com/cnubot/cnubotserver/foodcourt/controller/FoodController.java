2189ca0b4e40be763349f00117d590fbc330d219
kingseungwooya <ksw_0505@nate.com>
2022-11-21 15:21:14 +0900

    feat: create controller & service

---
+++ src/main/java/com/cnubot/cnubotserver/foodcourt/controller/FoodController.java
@@ -0,0 +1,19 @@
+package com.cnubot.cnubotserver.foodcourt.controller;
+
+import com.cnubot.cnubotserver.domain.entity.Board;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/food")
+@RequiredArgsConstructor
+public class FoodController {
+
+}
---
+++ src/main/java/com/cnubot/cnubotserver/foodcourt/service/FoodService.java
@@ -0,0 +1,7 @@
+package com.cnubot.cnubotserver.foodcourt.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface FoodService {
+}
