08915923deadef658375b0183599be285ae2305a
kingseungwooya <ksw_0505@nate.com>
2022-11-27 22:52:43 +0900

    fix: fix board scheduler

--- src/main/java/com/cnubot/cnubotserver/board/component/BoardScheduler.java
+++ src/main/java/com/cnubot/cnubotserver/board/component/BoardScheduler.java
@@ -12,7 +12,7 @@ public class BoardScheduler {
     private final BoardService boardService;
 
     // cron 방식으로 스케쥴링 1시간 간격으로 refresh
-    @Scheduled(cron = "0 0 12 1/1 * ? * ")
+    @Scheduled(cron = " 0 0 12 * * * ")
     public void refresh(){
         boardService.refreshBoards();
     }
