e985a64079fcf055e185595a2491d0c2eb45577e
kingseungwooya <ksw_0505@nate.com>
2022-11-27 22:42:37 +0900

    feat: add reposutory method

--- src/main/java/com/cnubot/cnubotserver/foodcourt/repository/MenuRepository.java
+++ src/main/java/com/cnubot/cnubotserver/foodcourt/repository/MenuRepository.java
@@ -1,9 +1,14 @@
 package com.cnubot.cnubotserver.foodcourt.repository;
 
 import com.cnubot.cnubotserver.foodcourt.entity.Menu;
+import com.cnubot.cnubotserver.foodcourt.enums.FoodCourt;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MenuRepository extends JpaRepository<Menu, String> {
+
+    List<Menu> findAllByFoodCourt(@Param("food_court") FoodCourt foodCourt);
 }
