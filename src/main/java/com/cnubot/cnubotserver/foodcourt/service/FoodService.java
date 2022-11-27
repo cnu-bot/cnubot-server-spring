df088556b023e79af9cc03174f9eb4189552bc7b
kingseungwooya <ksw_0505@nate.com>
2022-11-27 22:41:33 +0900

    feat: add refresh database feature

--- src/main/java/com/cnubot/cnubotserver/foodcourt/service/FoodService.java
+++ src/main/java/com/cnubot/cnubotserver/foodcourt/service/FoodService.java
@@ -1,7 +1,12 @@
 package com.cnubot.cnubotserver.foodcourt.service;
 
+import com.cnubot.cnubotserver.foodcourt.entity.Menu;
+import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface FoodService {
+
+    void refreshMenu();
+
 }
--- src/main/java/com/cnubot/cnubotserver/foodcourt/service/FoodServiceImpl.java
+++ src/main/java/com/cnubot/cnubotserver/foodcourt/service/FoodServiceImpl.java
@@ -1,9 +1,26 @@
 package com.cnubot.cnubotserver.foodcourt.service;
 
+import com.cnubot.cnubotserver.foodcourt.entity.Menu;
+import com.cnubot.cnubotserver.foodcourt.enums.FoodCourt;
+import com.cnubot.cnubotserver.foodcourt.repository.MenuRepository;
+import com.cnubot.cnubotserver.foodcourt.service.crawling.FoodCrawling;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
-public class FoodServiceImpl implements FoodService{
+@RequiredArgsConstructor
+public class FoodServiceImpl implements FoodService {
+
+    private final MenuRepository menuRepository;
+    private final FoodCrawling foodCrawling;
+
+    @Override
+    public void refreshMenu() {
+        menuRepository.deleteAllInBatch();
+        foodCrawling.process();
+    }
+    
 }
