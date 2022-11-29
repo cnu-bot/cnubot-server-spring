9aa296f3dda48a38c7f7d7c13ca7945aaef33fca
kingseungwooya <ksw_0505@nate.com>
2022-11-29 21:32:08 +0900

    test(foodcourt): change test

--- src/test/java/com/cnubot/cnubotserver/foodcourt/service/FirstHallServiceImplTest.java
+++ src/test/java/com/cnubot/cnubotserver/foodcourt/service/FirstHallServiceImplTest.java
@@ -6,30 +6,29 @@ import static org.junit.jupiter.api.Assertions.*;
 import com.cnubot.cnubotserver.foodcourt.entity.Menu;
 import com.cnubot.cnubotserver.foodcourt.enums.FoodCourt;
 import com.cnubot.cnubotserver.foodcourt.enums.Week;
+import com.cnubot.cnubotserver.foodcourt.repository.MenuRepository;
 import com.cnubot.cnubotserver.foodcourt.service.crawling.FoodCrawling;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
 class FirstHallServiceImplTest {
     @Autowired
     FoodService foodService;
     @Autowired
-    FoodCrawling foodCrawling;
-    @BeforeEach
-    void setUp() {
-        foodCrawling.process();
-        //foodService.refreshMenu();
-    }
+    MenuRepository repository;
 
     @Test
     void 크롤링_테스트() {
-        List<Menu> menus = foodService.getMenus(FoodCourt.DORMITORY.name(), Week.FRIDAY.name());
+        List<Menu> menus = foodService.getMenus(FoodCourt.DORMITORY.name(), "FRIDAY");
+
         System.out.println(menus);
-        assertThat(menus.size()).isEqualTo(3);
+        assertThat(menus.size()).isEqualTo(10);
 
     }
 }
