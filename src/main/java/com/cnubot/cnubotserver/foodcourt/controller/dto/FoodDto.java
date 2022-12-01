95c147660e5b20073c6cdf7847c977feabe1320e
kingseungwooya <ksw_0505@nate.com>
2022-12-01 16:51:28 +0900

    fix(foodcourt): fix dto

--- src/main/java/com/cnubot/cnubotserver/foodcourt/controller/dto/FoodDto.java
+++ src/main/java/com/cnubot/cnubotserver/foodcourt/controller/dto/FoodDto.java
@@ -3,13 +3,15 @@ package com.cnubot.cnubotserver.foodcourt.controller.dto;
 import com.cnubot.cnubotserver.foodcourt.enums.FirstHallType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class FoodDto {
 
-    private final String foodName;
-    private final FirstHallType type;
-    private final String price;
-
+    private String foodName;
+    private FirstHallType type;
+    private String price;
+    
 }
